330929692dffec7675aaee51c8d5e9011907e223
Bianca <bianca.rocha1711@gmail.com>
2018-12-02 22:01:58 -0300

    set up Input component and storeFactory

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from "redux";
-import success from "./successReducer";
+import { combineReducers } from 'redux';
+import success from '../reducers/successReducer/successReducer';
 
 export default combineReducers({
   success
--- src/testUtils.js
+++ src/testUtils.js
@@ -1,4 +1,20 @@
-import checkPropTypes from "check-prop-types";
+import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+
+import rootReducer from '../src/reducers';
+
+/**
+ * Create a testing store with imported reducers, middleware and initial state.
+ * globals: rootReducer
+ * @param {object} initialState - Initial state for store
+ * @function storeFactory
+ * @returns {Store} - Redux store
+ */
+
+export const storeFactory = initialState => {
+  return createStore(rootReducer, initialState);
+};
+
 /**
  * Return node(s) with the given data-test attribute
  * @param {ShallowWrapper} wrapper  - Enzyme shallow wrapper.
@@ -7,14 +23,14 @@ import checkPropTypes from "check-prop-types";
  */
 
 export const findByTestAttr = (wrapper, val) => {
-  return wrapper.find({ "data-test": val });
+  return wrapper.find({ 'data-test': val });
 };
 
 export const checkProps = (component, conformingProps) => {
   const propError = checkPropTypes(
     component.propTypes,
     conformingProps,
-    "prop",
+    'prop',
     component.name
   );
   expect(propError).toBeUndefined();
