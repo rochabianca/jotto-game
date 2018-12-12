4b1627e39e2ed42314935c5ee8712958374e86e3
Bianca <bianca.rocha1711@gmail.com>
2018-12-12 07:39:08 -0300

    tests for input success: true

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import { findByTestAttr, storeFactory } from '../../testUtils';
-import Input from './Input';
+import { findByTestAttr, storeFactory } from "../../testUtils";
+import Input from "./Input";
 
 /**
  * Factory function to create a ShallowWrapper for the GuessedWords component
@@ -17,36 +17,50 @@ const setup = (initialState = {}) => {
   return wrapper;
 };
 
-describe('render', () => {
-  describe('word has not been guessed', () => {
+describe("render", () => {
+  describe("word has not been guessed", () => {
     let wrapper;
     beforeEach(() => {
       const initialState = { success: false };
       wrapper = setup(initialState);
     });
-    test('renders component without error', () => {
-      const component = findByTestAttr(wrapper, 'component-input');
+    test("renders component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
       expect(component.length).toBe(1);
     });
 
-    test('renders input box', () => {
-      const inputBox = findByTestAttr(wrapper, 'input-box');
+    test("renders input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
       expect(inputBox.length).toBe(1);
     });
 
-    test('renders submit button', () => {
-      const submitButton = findByTestAttr(wrapper, 'submit-button');
+    test("renders submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
       expect(submitButton.length).toBe(1);
     });
   });
 
-  describe('word has been guessed', () => {
-    test('renders component without error', () => {});
+  describe("word has been guessed", () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+    test("renders component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
 
-    test('does not renders input box', () => {});
+    test("does not renders input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(0);
+    });
 
-    test('does not renders submit button', () => {});
+    test("does not renders submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(0);
+    });
   });
 });
 
-describe('update state', () => {});
+describe("update state", () => {});
