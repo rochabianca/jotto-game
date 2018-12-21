900ea3ace66288bc051b3f32142984a7dedf704e
Bianca <bianca.rocha1711@gmail.com>
2018-12-21 07:53:26 -0300

    helpers for get the letter match count (and tests)

---
+++ src/helpers/index.js
@@ -0,0 +1,13 @@
+/**
+ * @method getLetterMatchCount
+ * @param {string} guessedWord - Guessed word
+ * @param {string} secretWord  - Secret word
+ * @returns {number} - number of letters matched between guessed word and secret word
+ */
+
+export function getLetterMatchCount(guessedWord, secretWord) {
+  const secretLetterSet = new Set(secretWord.split(''));
+  const guessedLetterSet = new Set(guessedWord.split(''));
+  return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter))
+    .length;
+}
