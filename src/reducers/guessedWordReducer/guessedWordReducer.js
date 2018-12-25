8679e9c33136b7dfec4b3dc254fca018712270ea
Bianca <bianca.rocha1711@gmail.com>
2018-12-25 15:13:06 -0300

    add 'guessWord' action creator and thunk pierces of state

--- src/reducers/guessedWordReducer/guessedWordReducer.js
+++ src/reducers/guessedWordReducer/guessedWordReducer.js
@@ -1,3 +1,5 @@
+import { actionTypes } from '../../actions';
+
 /**
  * @function guessedWordsReducer
  * @param {array} state - Array of guessed words.
@@ -5,6 +7,11 @@
  * @returns {array} - New guessedWords state
  */
 
-export default (state, action) => {
-  return null;
+export default (state = [], action) => {
+  switch (action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import success from './successReducer/successReducer';
 import guessedWords from './guessedWordReducer/guessedWordReducer';
+import secretWord from './secretWordReducer/secretWordReducer';
 
 export default combineReducers({
+  secretWord,
   success,
   guessedWords
 });
