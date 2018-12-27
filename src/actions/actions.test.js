8fc3d0f8efaa7134b7a58774df8b951f4db8d90a
Bianca <bianca.rocha1711@gmail.com>
2018-12-26 22:12:37 -0300

    intalled axios and moxios and start the action tests

---
+++ src/actions/actions.test.js
@@ -0,0 +1,13 @@
+import moxios from 'moxios';
+
+describe('getSecretWord action creator', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  test('adds response word to state', () => {});
+});
